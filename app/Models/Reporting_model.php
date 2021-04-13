c3883e6504924693e29d3219d70c2e7c31b23c18
pejuangbitco <irsyadeinstein@gmail.com>
2021-04-13 17:19:38 +0700

    sync model with server

--- app/Models/Reporting_model.php
+++ app/Models/Reporting_model.php
@@ -9,27 +9,36 @@ class Reporting_model extends Model {
   protected $allowedFields = ['email', 'campaign_id', 'creative_id', 'creative_name', 'creative_size', 'inventory_id', 'inventory_name', 'exchange_name', 'impression', 'view', 'completed_view', 'click', 'win_rate', 'ctr', 'time'];
 
   public function getUserData($email) {
-    $sql = 'SELECT `creative_name`, `creative_size`, `inventory_name`, `exchange_name`,SUM(`impression`) as impression, SUM(`click`) AS click, SUM(`win_rate`) AS win_rate, SUM(`view`) AS view, SUM(`completed_view`) AS completed_view, `time`, `ctr` FROM `report` WHERE `email`=? GROUP BY `campaign_id` ORDER BY `time` ASC';
+    $sql = 'SELECT `id`, `campaign_id`, `creative_name`, `creative_size`, `inventory_name`, `exchange_name`,SUM(`impression`) as impression, SUM(`click`) AS click, SUM(`win_rate`) AS winrate, SUM(`view`) AS view, SUM(`completed_view`) AS completed_view, `time`, SUM(`ctr`) AS ctr FROM `report` WHERE `email`=? GROUP BY `campaign_id` ORDER BY `time` ASC';
     $result = $this->db->query($sql, $email);
     return $result->getResultArray();
   }
 
-  public function getInventoryName($campaign)
+  public function getInventoryName($campaign, $email)
   {
-    $result = $this->db->query("SELECT inventory_name, impression, click, ctr, win_rate, view, completed_view, a.campaign_name from report natural join (select DISTINCT campaign_id, campaign_name from dashboard) a where a.campaign_id=$campaign")->getResultArray();
-    return $result;
+    $sql = 'SELECT `id`, `inventory_name`, SUM(`impression`) as impression, SUM(`click`) AS click, SUM(`win_rate`) AS winrate, SUM(`view`) AS view, SUM(`completed_view`) AS completed_view, `time`, SUM(`ctr`) AS ctr FROM `report` WHERE `campaign_id`=? or `email`=? GROUP BY `inventory_name` ORDER BY `time` ASC';
+    $result = $this->db->query($sql, [$campaign, $email]);
+    return $result->getResultArray();
+    // $result = $this->db->query("SELECT inventory_name, impression, click, ctr, win_rate, view, completed_view, a.campaign_name from report natural join (select DISTINCT campaign_id, campaign_name from dashboard) a where a.campaign_id=$campaign or a.email=$email")->getResultArray();
+    // return $result;
   }
 
-  public function getCreativeName($campaign)
+  public function getCreativeName($campaign, $email)
   {
-    $result = $this->db->query("SELECT report.*, a.campaign_name from report natural join (select DISTINCT campaign_id, campaign_name from dashboard) a where a.campaign_id=$campaign")->getResultArray();
-    return $result;
+    $sql = 'SELECT `id`, `creative_name`, SUM(`impression`) as impression, SUM(`click`) AS click, SUM(`win_rate`) AS winrate, SUM(`view`) AS view, SUM(`completed_view`) AS completed_view, `time`, SUM(`ctr`) AS ctr FROM `report` WHERE `campaign_id`=? or `email`=? GROUP BY `creative_name` ORDER BY `time` ASC';
+    $result = $this->db->query($sql, [$campaign, $email]);
+    return $result->getResultArray();
+    // $result = $this->db->query("SELECT report.*, a.campaign_name from report natural join (select DISTINCT campaign_id, campaign_name from dashboard) a where a.campaign_id=$campaign")->getResultArray();
+    // return $result;
   }
 
-  public function getCampaignInformation($campaign)
+  public function getCampaignInformation($campaign, $email)
   {
-    $result = $this->db->query("SELECT * from dashboard where campaign_id=$campaign")->getResultArray();
-    return $result;
+    $sql = 'SELECT `id`, `campaign_id`, `campaign_name`,SUM(`impression`) as impression, SUM(`click`) AS click, SUM(`win_rate`) AS win_rate, SUM(`view`) AS view, SUM(`completed_view`) AS completed_view, `time`, SUM(`ctr`) AS ctr FROM `dashboard` WHERE `campaign_id`=? and `email`=?  GROUP BY `campaign_id` ORDER BY `time` ASC';
+    $result = $this->db->query($sql, [$campaign, $email]);
+    return $result->getResultArray();
+    // $result = $this->db->query("SELECT `id`, `campaign_id`, `campaign_name`,SUM(`impression`) as impression, SUM(`click`) AS click, SUM(`win_rate`) AS win_rate, SUM(`view`) AS view, SUM(`completed_view`) AS completed_view, `time`, SUM(`ctr`) AS ctr from dashboard where campaign_id=$campaign")->getResultArray();
+    // return $result;
   }
 
   public function getExchangeName($campaign)
