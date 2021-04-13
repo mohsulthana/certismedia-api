5a564751b798ac69de072dfb8e0eca5a7777fafe
pejuangbitco <irsyadeinstein@gmail.com>
2021-04-13 17:13:10 +0700

    register fetch shell exc

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -34,8 +34,7 @@ $routes->setAutoRoute(true);
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 $routes->post('/login', 'Authentication::login');
-$routes->post('/register', 'Authentication::register');
-
+$routes->post('/signup', 'Register::signup');
 $routes->resource('dashboard');
 $routes->resource('reporting');
 
